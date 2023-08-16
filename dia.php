9936a7d27026189ff85408d7d177e6ced2cfe440
lan123456ok <bachomuonnam123@gmail.com>
2023-08-16 16:25:51 +0900

    simple MVC CRUD

--- dia.php
+++
@@ -1,4 +0,0 @@
-Đĩa
-
-<?php echo $each['ten_mon_an']; ?>
-<img src="<?php echo $each['anh']; ?>">
---
+++ view/edit.php
@@ -0,0 +1,9 @@
+<form action="?action=update" method="post">
+    <input type="hidden" name="id" value="<?php echo $each['ma']; ?>">
+    Tên
+    <input type="text" name="ten" value="<?php echo $each['ten']; ?>">
+    <br>
+    <button>
+        Sữa
+    </button>
+</form>
